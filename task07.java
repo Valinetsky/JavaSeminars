3c7fc3ac25f104c7e3be2c436b273bef2a62140e
Терёшин Роман <rtereshin@mac-55450192.users.rbc.ru>
2023-03-27 22:02:30 +0300

    Seminar01. All

--- task07.java
+++ task07.java
@@ -22,7 +22,7 @@ Sample Output:
 1
 */
 
-import java.util.Scanner;
+import java.util.*;
 
 public class task07 {
 
@@ -37,15 +37,34 @@ public class task07 {
 		}
 	}
 
-	public static void main(String[] args) {
-
-		int arr_size = getInt("Введите размер массива: ");
-
-		int[] myArray = new int[arr_size];
-
-		for (int i = 0; i < myArray.length; i++) {
-			myArray[i] = getInt("Введите элемент массива: ");
+	// Общий метод для проверки дубликатов в массиве
+	static void checkForDuplicates(int[] array)
+	{
+		// для каждого элемента массива проверяем, встречается ли он потом в массиве
+		for (int i = 0; i < array.length; i++)
+		{
+			int j = 0;
+			boolean flag = true;
+			boolean count_double = false;
+			while (flag) {
+				if (array[j] == array[i] && i != j) {
+					count_double = true;
+					flag = false;
+				}
+				j++;
+				if (j >= array.length) {
+					flag = false;
+				}
+			}
+			// System.out.println(count_double);
+			if (!count_double) {
+				System.out.println(array[i]);
+			}
 		}
+ 	}
 
+	public static void main(String[] args) {
+		int[] myArray = new int[] {8, 1, 2, 2, 3, 3, 3, 5, 7, 12, 12, 13, 4, 4};
+		checkForDuplicates(myArray);
 	}
 }
