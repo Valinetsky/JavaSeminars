9cc49125c20719fcf69d65c26d9fbade3b651a46
Valinetsky <highrat@gmail.com>
2023-04-10 14:20:55 +0300

    Seminar05. Task04 start.

--- Seminar05/Task03.java
+++ Seminar05/Task03.java
@@ -9,17 +9,23 @@
 
 package Seminar05;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Task03 {
 
 	public static void main(String[] args) {
 		int result = 0;
-		int[] myArr = { 1, 1, 1, 2, 2 };
-		for (int i = 0; i < myArr.length; i++) {
-			if (myArr) {
+		List<Integer> myArr = Arrays.asList(1, 1, 1, 2, 2, 3, 3, 3);
 
+		for (int i = 0; i < myArr.size() - 1; i++) {
+			for (int j = i + 1; j < myArr.size(); j++) {
+				if (myArr.get(i) == myArr.get(j)) {
+					result++;
+				}
 			}
 		}
-
+		System.out.println(myArr);
+		System.out.println(result);
 	}
-
 }
---
+++ Task04.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+
+/*
+Массив путей
+Показать путь
+
+[["Moscow", "Samara"], ["Kursk", "Pensa"], ["Samara", "Kursk"]]
+
+Pensa
+*/
+
+public class Task04 {
+	public static void main(String[] args) {
+		str[]
+	}
+
+	public static String getFinalCity(Map<String, String> map) {
+	}
+}
