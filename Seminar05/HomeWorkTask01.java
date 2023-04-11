4a81ca9cbcd5e5525c7202bcb80e56db780c61b4
Valinetsky <highrat@gmail.com>
2023-04-11 15:50:20 +0300

    Seminar05. Homework start.

---
+++ Seminar05/HomeWorkTask01.java
@@ -0,0 +1,42 @@
+package Seminar05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HomeWorkTask01 {
+
+	// добавляем номера в телефонную книгу
+	public static void addNumber(String key, int value, Map<String, ArrayList<Integer>> map) {
+		if (map.containsKey(key)) {
+			map.get(key).add(value);
+		} else {
+			ArrayList<Integer> list = new ArrayList<>();
+			list.add(value);
+			map.put(key, list);
+		}
+	}
+
+	// выводим телефонную книгу
+	public static void printBook(Map<String, ArrayList<Integer>> map) {
+		for (var item : map.entrySet()) {
+			String phones = "";
+			for (int element : item.getValue()) {
+				phones = phones + element + " ";
+			}
+			phones = phones.trim().replaceAll(" ", ", ");
+			System.out.printf("%s: %s \n", item.getKey(), phones);
+		}
+	}
+
+	public static void main(String[] args) {
+		Map<String, ArrayList<Integer>> bookPhone = new HashMap<>();
+		addNumber("Saveliev", 54892, bookPhone);
+		addNumber("Novikov", 11111111, bookPhone);
+		addNumber("Lobanov", 222, bookPhone);
+		addNumber("Saveliev", 634561111, bookPhone);
+		addNumber("Saveliev", 6345646, bookPhone);
+		addNumber("Saveliev", 02546, bookPhone);
+		printBook(bookPhone);
+	}
+}
