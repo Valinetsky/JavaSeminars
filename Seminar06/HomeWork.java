29e106b9aa5c9f34d69b8aa56d6905b3ea0c375d
Valinetsky <highrat@gmail.com>
2023-04-17 14:03:45 +0300

    Seminar06. Finished.

--- Seminar06/HomeWork.java
+++ Seminar06/HomeWork.java
@@ -9,56 +9,57 @@ import java.util.Set;
 
 public class HomeWork {
 
-    
-    public static void main(String[] args) throws Exception {
-        
-        Notebook notebook1 = new Notebook("Lenovo IdealPad 5", 8, 256, "Windows 10", "red", 15.6);
-        Notebook notebook2 = new Notebook("Honor MagicBook 16", 16, 512, "no OS", "green", 16.1);
-        Notebook notebook3 = new Notebook("Apple MacBook Air 13", 8, 256, "MacOs", "blue", 13.3);
-        Notebook notebook4 = new Notebook("HP 250 G7", 4, 1024, "no OS", "gray", 15.6);
-        Notebook notebook5 = new Notebook("Xiomi RedmiBook 15", 8, 256, "Windows 11", "black", 15.6);
-        Notebook notebook6 = notebook1;
+	public static void main(String[] args) throws Exception {
 
-        Set<Notebook> unicNotebook = new HashSet<Notebook>();
-        unicNotebook.add(notebook1);
-        unicNotebook.add(notebook2);
-        unicNotebook.add(notebook3);
-        unicNotebook.add(notebook4);
-        unicNotebook.add(notebook5);
-        unicNotebook.add(notebook6);
-        
-        System.out.printf("Unique notebooks, total: %d \n", unicNotebook.size());
+		Notebook notebook1 = new Notebook("Lenovo IdealPad 5", 32, 256, "Windows 10", "red", 15.6);
+		Notebook notebook2 = new Notebook("Honor MagicBook 16", 16, 512, "no OS", "green", 16.1);
+		Notebook notebook3 = new Notebook("Apple MacBook Air 13", 8, 256, "MacOs", "blue", 13.3);
+		Notebook notebook4 = new Notebook("HP 250 G7", 4, 1024, "no OS", "gray", 15.6);
+		Notebook notebook5 = new Notebook("Xiomi RedmiBook 15", 8, 256, "Windows 11", "black", 15.6);
+		Notebook notebook6 = notebook1;
 
-        for (Notebook notebook : unicNotebook) {
-            System.out.println();
-            System.out.println(notebook);
-        }
-        
+		Set<Notebook> unicNotebook = new HashSet<Notebook>();
+		unicNotebook.add(notebook1);
+		unicNotebook.add(notebook2);
+		unicNotebook.add(notebook3);
+		unicNotebook.add(notebook4);
+		unicNotebook.add(notebook5);
+		unicNotebook.add(notebook6);
 
-        Map<Integer, String> mapCrit = new HashMap<>();
-        mapCrit.put(1, "RAM size");
-        mapCrit.put(2, "HDD (or SSD) size");
-        mapCrit.put(3, "OS");
-        mapCrit.put(4, "Colour");
-        mapCrit.put(5, "Display size");
+		System.out.printf("Unique notebooks in store, total: %d \n", unicNotebook.size());
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input minimal parameter for search \n1. RAM size: ");
-        int ramUser = sc.nextInt();
-        
-        System.out.println("HDD (or SSD) size: ");
-        int storUser = sc.nextInt();
-        
-        System.out.println("Display size");
-        double digUser = sc.nextDouble();
+		for (Notebook notebook : unicNotebook) {
+			System.out.println();
+			System.out.println(notebook);
+		}
 
-        System.out.println();
-        
-        for(Notebook note: unicNotebook) {
-            if ((note.getRam() >= ramUser) && (note.getStorageCap() >= storUser)  && note.getDiagonal() >= digUser) {
-                System.out.println(note.toString());
-            }
-        }
-        sc.close();
-    }
+		Map<Integer, String> mapCrit = new HashMap<>();
+		mapCrit.put(1, "RAM size");
+		mapCrit.put(2, "HDD (or SSD) size");
+		mapCrit.put(3, "OS");
+		mapCrit.put(4, "Colour");
+		mapCrit.put(5, "Display size");
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input minimal parameter for search \n1. RAM size: ");
+		int ramUser = sc.nextInt();
+
+		System.out.println("HDD (or SSD) size: ");
+		int storUser = sc.nextInt();
+
+		System.out.println("Display size");
+		double digUser = sc.nextDouble();
+
+		System.out.println();
+		System.out.println("--------------");
+		System.out.println("Search result: ");
+		System.out.println();
+
+		for (Notebook note : unicNotebook) {
+			if ((note.getRam() >= ramUser) && (note.getStorageCap() >= storUser) && note.getDiagonal() >= digUser) {
+				System.out.println(note.toString());
+			}
+		}
+		sc.close();
+	}
 }
--- Seminar06/Notebook.java
+++ Seminar06/Notebook.java
@@ -1,52 +1,56 @@
 package Seminar06;
 
 public class Notebook {
-    //public static int counter = 0;
-    //private int id;
-    private String name;
-    private int ram;
-    private int hdd;
-    private String os;
-    private String colour;
-    private double diagonal;
+	// public static int counter = 0;
+	// private int id;
+	private String name;
+	private int ram;
+	private int hdd;
+	private String os;
+	private String colour;
+	private double diagonal;
 
-    public Notebook(String name, int ram, int hdd, String os, String colour, double diagonal){
-        //this.id = counter++;
-        this.name = name;
-        this.ram = ram;
-        this.hdd = hdd;
-        this.os = os;
-        this.colour = colour;
-        this.diagonal = diagonal;
-    }
+	public Notebook(String name, int ram, int hdd, String os, String colour, double diagonal) {
+		// this.id = counter++;
+		this.name = name;
+		this.ram = ram;
+		this.hdd = hdd;
+		this.os = os;
+		this.colour = colour;
+		this.diagonal = diagonal;
+	}
 
-    @Override
-    public String toString() {
-        return String.format("Name: %s \n RAM: %d Gb \n HDD(or SSD): %d Gb \n OS: %s \n Colour: %s \n Display size: %.1f \n", 
-                    this.name, this.ram, this.hdd, this.os, this.colour, this.diagonal);
-    }
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Notebook) {
-            return this.name.equals(((Notebook) obj).name) && this.ram == ((Notebook) obj).ram 
-                                && this.hdd == ((Notebook) obj).hdd && this.os.equals(((Notebook) obj).os) 
-                                && this.colour.equals(((Notebook) obj).colour) && this.diagonal == ((Notebook) obj).diagonal;
-        }
-        return false;
-        
-    }
+	@Override
+	public String toString() {
+		return String.format(
+				"Name: %s \n RAM: %d Gb \n HDD(or SSD): %d Gb \n OS: %s \n Colour: %s \n Display size: %.1f \n",
+				this.name, this.ram, this.hdd, this.os, this.colour, this.diagonal);
+	}
 
-    public int getRam(){
-        return this.ram;
-    }
-    public int getStorageCap(){
-        return this.hdd;
-    }
-    public String getOS(){
-        return this.os;
-    }
-    public double getDiagonal(){
-        return this.diagonal;
-    }
-    
-} 
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Notebook) {
+			return this.name.equals(((Notebook) obj).name) && this.ram == ((Notebook) obj).ram
+					&& this.hdd == ((Notebook) obj).hdd && this.os.equals(((Notebook) obj).os)
+					&& this.colour.equals(((Notebook) obj).colour) && this.diagonal == ((Notebook) obj).diagonal;
+		}
+		return false;
+	}
+
+	public int getRam() {
+		return this.ram;
+	}
+
+	public int getStorageCap() {
+		return this.hdd;
+	}
+
+	public String getOS() {
+		return this.os;
+	}
+
+	public double getDiagonal() {
+		return this.diagonal;
+	}
+
+}
